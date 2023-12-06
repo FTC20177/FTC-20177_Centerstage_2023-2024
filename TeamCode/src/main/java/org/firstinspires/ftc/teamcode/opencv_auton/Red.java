1ac84198d2cf430f4e64900b5c9cc77d1e4575b1
FTC 20177 High Velocity Robotics <highvelocity20177@covesd.org>
2023-12-05 19:12:56 -0800

    Fixed Building Issues and Implemented Custom Object Detection

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opencv_auton/Red.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opencv_auton/Red.java
@@ -48,14 +48,14 @@ import java.util.List;
  * Use Android Studio to Copy this Class, and Paste it into your team's code folder with a new name.
  * Remove or comment out the @Disabled line to add this OpMode to the Driver Station OpMode list.
  */
-@Autonomous(name = "Blue Cat Detection", group = "Concept")
-public class Blue extends LinearOpMode {
+@Autonomous(name = "Red Cat Detection", group = "Concept")
+public class Red extends LinearOpMode {
 
     private static final boolean USE_WEBCAM = true;  // true for webcam, false for phone camera
 
     // TFOD_MODEL_ASSET points to a model file stored in the project Asset location,
     // this is only used for Android Studio when using models in Assets.
-    private static final String TFOD_MODEL_ASSET = "model_20231204_135821.tflite";
+    private static final String TFOD_MODEL_ASSET = "model_20231204_155444.tflite";
     // TFOD_MODEL_FILE points to a model file stored onboard the Robot Controller's storage,
     // this is used when uploading models directly to the RC using the model upload interface.
     //private static final String TFOD_MODEL_FILE = "/sdcard/FIRST/tflitemodels/model_20231204_135821.tflite";
